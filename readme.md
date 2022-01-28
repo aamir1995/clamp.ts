88fd620e90cd90b100924628a1b110cdc69cd473
aamir1995 <syedaamir974@gmail.com>
2022-01-28 17:24:26 +0500

    Fix: minor fixes, docs and license.

---
+++ readme.md
@@ -0,0 +1,37 @@
+![NPM release](https://github.com/aamir1995/clamp.ts/actions/workflows/release.yml/badge.svg)
+![version on NPM](https://img.shields.io/npm/v/clamp.ts)
+![total downloads](https://img.shields.io/npm/dt/clamp.ts)
+![bundle size](https://img.shields.io/bundlephobia/minzip/clamp.ts)
+![license](https://img.shields.io/github/license/aamir1995/clamp.ts)
+
+# clamp.ts
+TypeScript fork of [clamp.js](https://github.com/josephschmitt/Clamp.js)
+
+Clamps (ie. cuts off) an HTML element's content by adding ellipsis to it if the 
+content inside is too long.
+
+ Demo: https://stackblitz.com/edit/typescript-zi38tc?file=index.ts
+
+## Sample Usage
+The `clamp` method is the primary way of interacting with Clamp.ts, and it takes two
+arguments. The first is the element which should be clamped, and the second is an
+Object with options in JSON notation.
+```
+import { clamp } from  'clamp.ts';
+
+const  paragraph = document.querySelector('#paragraph');
+
+clamp(paragraph, { // options here, read below for more info });
+```
+
+
+## Options
+
+| Property | Type | Default | Description |
+|--|--|--|--|
+| clamp | `number|string|‘auto’` | `2` | This controls where and when to clamp the text of an element. Submitting a number controls the number of lines that should be displayed. Second, you can submit a CSS value (in px or em) that controls the height of the element as a String. Finally, you can submit the word `'auto'` as a string. Auto will try to fill up the available space with the content and then automatically clamp once content no longer fits. This last option should only be set if a static  height is being set on the element elsewhere (such as through CSS) otherwise no  clamping will be done. |
+| useNativeClamp | boolean | `true` | Enables or disables using the native `-webkit-line-clamp` in a supported browser (ie. Webkit). It defaults to true if you're using Webkit, but it can behave wonky sometimes so you can set it to false to use the JavaScript-based solution. |
+| truncationChar | string | `…` | The character to insert at the end of the HTML element after truncation is performed. This defaults to an ellipsis (…). |
+| truncationHTML | string || A string of HTML to insert before the truncation character. This is useful if you'd like to add a "Read more" link or some such thing at the end of your clamped node. |
+| splitOnChars | Array | `['.', '-', '–', '—', ' ']` | Determines what characters to use to chunk an element into smaller pieces. you have an option to pass a list of characters it can use. For example, it you pass an array of `['.', ',', ' ']` then it will first remove sentences, then remove comma-phrases, and remove words, and finally remove individual characters to try and find the correct height. This will lead to increased performance and less looping when removing larger pieces of text (such as in paragraphs). The default is set to remove sentences (periods), hypens, en-dashes, em-dashes, and finally words  (spaces). Removing by character is always enabled as the last attempt no matter what is submitted in the array. |
+| animate | Boolean | `false` | Silly little easter-egg that, when set to true, will animate removing individual characters from the end of the element until the content fits. Defaults to `false`. |
--- src/clamp.ts
+++ src/clamp.ts
@@ -6,27 +6,26 @@
  * Ported to TypeScript by Aamir Shah github.com/aamir1995
  */
 
-
 /********************************************************
-*                                                       *
-*  INTERFACES                                           *
-*                                                       *
-*********************************************************/
+ *                                                       *
+ *  INTERFACES                                           *
+ *                                                       *
+ *********************************************************/
 
 interface Options {
-    clamp?: number | string | 'auto';
-    useNativeClamp?: boolean;
-    splitOnChars?: Array<string>;
-    animate?: boolean;
-    truncationChar?: string;
-    truncationHTML?: string
+  clamp?: number | string | 'auto';
+  useNativeClamp?: boolean;
+  splitOnChars?: Array<string>;
+  animate?: boolean;
+  truncationChar?: string;
+  truncationHTML?: string;
 }
 
 /********************************************************
-*                                                       *
-*  UTILITY FUNCTIONS                                    *
-*                                                       *
-*********************************************************/
+ *                                                       *
+ *  UTILITY FUNCTIONS                                    *
+ *                                                       *
+ *********************************************************/
 
 /**
  * @description Return the current style for an element.
@@ -34,34 +33,33 @@ interface Options {
  * @param prop The style property.
  * @returns CSS property values.
  */
-const computeStyle = (elem: HTMLElement, prop: string): string => {
+const computeStyle = (elem: HTMLElement | Element, prop: string): string => {
+  const win: Window = window;
 
-    const win: Window = window;
-
-    /**
-     * @todo re-enable if needed
-     */
-    // if (!win.getComputedStyle) {
-    //     win.getComputedStyle = (el: Element, pseudo) => {
-    //         // this.el = el;
-    //         return {
-    //             getPropertyValue(prop) {
-    //                 var re = /(\-([a-z]){1})/g;
-    //                 if (prop == 'float')
-    //                     prop = 'styleFloat';
-    //                 if (re.test(prop)) {
-    //                     prop = prop.replace(re, function () {
-    //                         return arguments[2].toUpperCase();
-    //                     });
-    //                 }
-    //                 return el.currentStyle && el.currentStyle[prop] ? el.currentStyle[prop] : null;
-    //             }
-    //         }
-    //         // return get
-    //     };
-    // }
-    return win.getComputedStyle(elem, null).getPropertyValue(prop);
-}
+  /**
+   * @todo re-enable if needed
+   */
+  // if (!win.getComputedStyle) {
+  //     win.getComputedStyle = (el: Element, pseudo) => {
+  //         // this.el = el;
+  //         return {
+  //             getPropertyValue(prop) {
+  //                 var re = /(\-([a-z]){1})/g;
+  //                 if (prop == 'float')
+  //                     prop = 'styleFloat';
+  //                 if (re.test(prop)) {
+  //                     prop = prop.replace(re, function () {
+  //                         return arguments[2].toUpperCase();
+  //                     });
+  //                 }
+  //                 return el.currentStyle && el.currentStyle[prop] ? el.currentStyle[prop] : null;
+  //             }
+  //         }
+  //         // return get
+  //     };
+  // }
+  return win.getComputedStyle(elem, null).getPropertyValue(prop);
+};
 
 /**
  * @description Returns the maximum number of lines of text that should be rendered based
@@ -70,60 +68,72 @@ const computeStyle = (elem: HTMLElement, prop: string): string => {
  * @param height element's height
  * @returns max lines
  */
-const getMaxLines = (element: HTMLElement, height?: number): number => {
-    const availHeight = height || element.clientHeight, lineHeight = getLineHeight(element);
-    return Math.max(Math.floor(availHeight / lineHeight), 0);
-}
+const getMaxLines = (element: HTMLElement | Element, height?: number): number => {
+  const availHeight = height || element.clientHeight,
+    lineHeight = getLineHeight(element);
+  return Math.max(Math.floor(availHeight / lineHeight), 0);
+};
 
 /**
  * @description Returns the maximum height a given element should have based on the line-
  * height of the text and the given clamp value.
- * 
+ *
  * @param element any HTML element
  * @param clmp number of clamps
  * @returns max height
  */
-const getMaxHeight = (element: HTMLElement, clmp: number): number => {
-    const lineHeight = getLineHeight(element);
-    return lineHeight * clmp;
-}
+const getMaxHeight = (element: HTMLElement | Element, clmp: number): number => {
+  const lineHeight = getLineHeight(element);
+  return lineHeight * clmp;
+};
 
 /**
  * @description Returns the line-height of an element as an integer.
  * @param elem any HTML Element
  * @returns line-height of the element
  */
-const getLineHeight = (elem: HTMLElement): number => {
-    const lh = computeStyle(elem, 'line-height');
-    if (lh == 'normal') {
-        // Normal line heights vary from browser to browser. The spec recommends
-        // a value between 1.0 and 1.2 of the font size. Using 1.1 to split the diff.
-        return parseInt(computeStyle(elem, 'font-size')) * 1.2;
-    }
-    return parseInt(lh);
-}
+const getLineHeight = (elem: HTMLElement | Element): number => {
+  const lh = computeStyle(elem, 'line-height');
+  if (lh == 'normal') {
+    // Normal line heights vary from browser to browser. The spec recommends
+    // a value between 1.0 and 1.2 of the font size. Using 1.1 to split the diff.
+    return parseInt(computeStyle(elem, 'font-size')) * 1.2;
+  }
+  return parseInt(lh);
+};
 
 /**
  * @description Gets an element's last child. That may be another node or a node's contents.
  * @param elem any HTML Element
- * @param options config option 
+ * @param options config option
  * @returns Element's last child.
  */
-const getLastChild = (elem: HTMLElement, options: Options): HTMLElement => {
-    //Current element has children, need to go deeper and get last child as a text node
-    if ((elem.lastChild as any).children && (elem.lastChild as any).children.length > 0) {
-        return getLastChild(Array.prototype.slice.call(elem.children).pop(), options);
-    }
-    //This is the absolute last child, a text node, but something's wrong with it. Remove it and keep trying
-    else if (!elem.lastChild || !elem.lastChild.nodeValue || elem.lastChild.nodeValue === '' || elem.lastChild.nodeValue == options.truncationChar) {
-        elem.lastChild.parentNode.removeChild(elem.lastChild);
-        return getLastChild(elem, options);
-    }
-    //This is the last child we want, return it
-    else {
-        return (elem.lastChild as HTMLElement);
-    }
-}
+const getLastChild = (elem: HTMLElement | Element, options: Options): HTMLElement => {
+  //Current element has children, need to go deeper and get last child as a text node
+  if (
+    (elem.lastChild as any).children &&
+    (elem.lastChild as any).children.length > 0
+  ) {
+    return getLastChild(
+      Array.prototype.slice.call(elem.children).pop(),
+      options
+    );
+  }
+  //This is the absolute last child, a text node, but something's wrong with it. Remove it and keep trying
+  else if (
+    !elem.lastChild ||
+    !elem.lastChild.nodeValue ||
+    elem.lastChild.nodeValue === '' ||
+    elem.lastChild.nodeValue == options.truncationChar
+  ) {
+    elem.lastChild.parentNode.removeChild(elem.lastChild);
+    return getLastChild(elem, options);
+  }
+  //This is the last child we want, return it
+  else {
+    return elem.lastChild as HTMLElement;
+  }
+};
 
 /**
  * @description Applies ellipsis to the provided string.
@@ -131,184 +141,211 @@ const getLastChild = (elem: HTMLElement, options: Options): HTMLElement => {
  * @param str string that needs to be truncated
  * @param options config option
  */
-const applyEllipsis = (elem: HTMLElement, str: string, options: Options): void => {
-    elem.nodeValue = str + options.truncationChar;
-}
-
+const applyEllipsis = (
+  elem: HTMLElement | Element,
+  str: string,
+  options: Options
+): void => {
+  elem.nodeValue = str + options.truncationChar;
+};
 
 /**
  * @description Removes one character at a time from the text until its width or
  * height is beneath the passed-in max param.
  * @param target
- * @param element 
- * @param truncationHTMLContainer 
- * @param maxHeight 
- * @param options 
- * @param config 
+ * @param element
+ * @param truncationHTMLContainer
+ * @param maxHeight
+ * @param options
+ * @param config
  * @returns innerHTML
  */
 const truncate = (
-    target: HTMLElement,
-    element: HTMLElement,
-    truncationHTMLContainer: HTMLElement,
-    maxHeight: number,
-    options: Options, config: any = {
-        splitOnChars: options.splitOnChars.slice(0),
-        splitChar: options.splitOnChars.slice(0)[0],
-        chunks: null,
-        lastChunk: null,
-    }): string => {
-    if (!maxHeight) {
-        return element.innerHTML;
-    }
-
-    let nodeValue = target.nodeValue.replace(options.truncationChar, '');
-
-    let {
-        splitOnChars,
-        splitChar,
-        chunks,
-        lastChunk
-    } = config;
-
-    //Grab the next chunks
-    if (!chunks) {
-        //If there are more characters to try, grab the next one
-        if (splitOnChars.length > 0) {
-            splitChar = splitOnChars.shift();
-        }
-        //No characters to chunk by. Go character-by-character
-        else {
-            splitChar = '';
-        }
-        chunks = nodeValue.split(splitChar);
-    }
-    //If there are chunks left to remove, remove the last one and see if
-    // the nodeValue fits.
-    if (chunks.length > 1) {
-        // console.log('chunks', chunks);
-        lastChunk = chunks.pop();
-        // console.log('lastChunk', lastChunk);
-        applyEllipsis(target, chunks.join(splitChar), options);
-    }
-    //No more chunks can be removed using this character
-    else {
-        chunks = null;
-    }
-    // Insert the custom HTML before the truncation character
-    if (truncationHTMLContainer) {
-        target.nodeValue = target.nodeValue.replace(options.truncationChar, '');
-        element.innerHTML = target.nodeValue + ' ' + truncationHTMLContainer.innerHTML + options.truncationChar;
-    }
-    // Search produced valid chunks
-    if (chunks) {
-        // It fits
-        if (element.clientHeight <= maxHeight) {
-            // There's still more characters to try splitting on, not quite done yet
-            if (splitOnChars.length >= 0 && splitChar !== '') {
-                applyEllipsis(target, chunks.join(splitChar) + splitChar + lastChunk, options);
-                chunks = null;
-            }
-            // Finished!
-            else {
-                return element.innerHTML;
-            }
-        }
-    }
-    // No valid chunks produced
-    else {
-        // No valid chunks even when splitting by letter, time to move
-        // on to the next node
-        if (splitChar === '') {
-
-            applyEllipsis(target, '', options);
-            target = getLastChild(element, options);
-
-            // reset vars
-            splitOnChars = options.splitOnChars.slice(0);
-            splitChar = splitOnChars[0];
-            chunks = null;
-            lastChunk = null;
-        }
-    }
-    // If you get here it means still too big, let's keep truncating
-    if (options.animate) {
-        setTimeout(() => {
-            truncate(target, element, truncationHTMLContainer, maxHeight, options, { splitOnChars, splitChar, chunks, lastChunk });
-        }, options.animate === true ? 10 : options.animate);
-    }
-    else {
-        return truncate(target, element, truncationHTMLContainer, maxHeight, options, { splitOnChars, splitChar, chunks, lastChunk });
-    }
-
-    // just to suppress TS warning...
+  target: HTMLElement | Element,
+  element: HTMLElement | Element,
+  truncationHTMLContainer: HTMLElement,
+  maxHeight: number,
+  options: Options,
+  config: any = {
+    splitOnChars: options.splitOnChars.slice(0),
+    splitChar: options.splitOnChars.slice(0)[0],
+    chunks: null,
+    lastChunk: null,
+  }
+): string => {
+  if (!maxHeight) {
     return element.innerHTML;
+  }
 
-}
+  let nodeValue = target.nodeValue.replace(options.truncationChar, '');
+
+  let { splitOnChars, splitChar, chunks, lastChunk } = config;
+
+  //Grab the next chunks
+  if (!chunks) {
+    //If there are more characters to try, grab the next one
+    if (splitOnChars.length > 0) {
+      splitChar = splitOnChars.shift();
+    }
+    //No characters to chunk by. Go character-by-character
+    else {
+      splitChar = '';
+    }
+    chunks = nodeValue.split(splitChar);
+  }
+  //If there are chunks left to remove, remove the last one and see if
+  // the nodeValue fits.
+  if (chunks.length > 1) {
+    // console.log('chunks', chunks);
+    lastChunk = chunks.pop();
+    // console.log('lastChunk', lastChunk);
+    applyEllipsis(target, chunks.join(splitChar), options);
+  }
+  //No more chunks can be removed using this character
+  else {
+    chunks = null;
+  }
+  // Insert the custom HTML before the truncation character
+  if (truncationHTMLContainer) {
+    target.nodeValue = target.nodeValue.replace(options.truncationChar, '');
+    element.innerHTML =
+      target.nodeValue +
+      ' ' +
+      truncationHTMLContainer.innerHTML +
+      options.truncationChar;
+  }
+  // Search produced valid chunks
+  if (chunks) {
+    // It fits
+    if (element.clientHeight <= maxHeight) {
+      // There's still more characters to try splitting on, not quite done yet
+      if (splitOnChars.length >= 0 && splitChar !== '') {
+        applyEllipsis(
+          target,
+          chunks.join(splitChar) + splitChar + lastChunk,
+          options
+        );
+        chunks = null;
+      }
+      // Finished!
+      else {
+        return element.innerHTML;
+      }
+    }
+  }
+  // No valid chunks produced
+  else {
+    // No valid chunks even when splitting by letter, time to move
+    // on to the next node
+    if (splitChar === '') {
+      applyEllipsis(target, '', options);
+      target = getLastChild(element, options);
+
+      // reset vars
+      splitOnChars = options.splitOnChars.slice(0);
+      splitChar = splitOnChars[0];
+      chunks = null;
+      lastChunk = null;
+    }
+  }
+  // If you get here it means still too big, let's keep truncating
+  if (options.animate) {
+    setTimeout(
+      () => {
+        truncate(target, element, truncationHTMLContainer, maxHeight, options, {
+          splitOnChars,
+          splitChar,
+          chunks,
+          lastChunk,
+        });
+      },
+      options.animate === true ? 10 : options.animate
+    );
+  } else {
+    return truncate(
+      target,
+      element,
+      truncationHTMLContainer,
+      maxHeight,
+      options,
+      { splitOnChars, splitChar, chunks, lastChunk }
+    );
+  }
+
+  // just to suppress TS warning...
+  return element.innerHTML;
+};
 
 
 /********************************************************
-*                                                       *
-*  EXPORTED METHOD                                      *
-*                                                       *
-*********************************************************/
+ *                                                       *
+ *  EXPORTED METHOD                                      *
+ *                                                       *
+ *********************************************************/
 
 /**
  * @description Clamps a text node.
  * @param element. Element containing the text node to clamp.
  * @param options. Options to pass to the clamper.
  */
-export function clamp(element: HTMLElement, options?: Options) {
+export function clamp(element: Element | HTMLElement, options?: Options) {
+  /**
+   * merge default options with provided options (if any).
+   */
+  options = {
+    clamp: 2,
+    useNativeClamp: true,
+    splitOnChars: ['.', '-', '–', '—', ' '],
+    animate: false,
+    truncationChar: '…',
+    ...options,
+  };
 
-    /**
-     * merge default options with provided options (if any).
-     */
-    options = {
-        clamp: 2,
-        useNativeClamp: true,
-        splitOnChars: ['.', '-', '–', '—', ' '],
-        animate: false,
-        truncationChar: '…',
-        ...options
-    };
+  const sty = (<HTMLElement>element).style;
+  const original = element.innerHTML;
+  const supportsNativeClamp =
+    typeof (<HTMLElement>element).style.webkitLineClamp != 'undefined';
+  let clampValue = options.clamp;
+  const isCSSValue =
+    (clampValue as string).indexOf &&
+    ((clampValue as string).indexOf('px') > -1 ||
+      (clampValue as string).indexOf('em') > -1);
+  let truncationHTMLContainer;
+  if (options.truncationHTML) {
+    truncationHTMLContainer = document.createElement('span');
+    truncationHTMLContainer.innerHTML = options.truncationHTML;
+  }
 
-    const sty = element.style;
-    const original = element.innerHTML;
-    const supportsNativeClamp = typeof (element.style.webkitLineClamp) != 'undefined';
-    let clampValue = options.clamp;
-    const isCSSValue = (clampValue as string).indexOf && ((clampValue as string).indexOf('px') > -1 || (clampValue as string).indexOf('em') > -1);
-    let truncationHTMLContainer;
-    if (options.truncationHTML) {
-        truncationHTMLContainer = document.createElement('span');
-        truncationHTMLContainer.innerHTML = options.truncationHTML;
+  // CONSTRUCTOR ________________________________________________________________
+  if (clampValue == 'auto') {
+    clampValue = getMaxLines(element);
+  } else if (isCSSValue) {
+    clampValue = getMaxLines(element, parseInt(clampValue as string));
+  }
+  let clamped: string;
+  if (supportsNativeClamp && options.useNativeClamp) {
+    sty.overflow = 'hidden';
+    sty.textOverflow = 'ellipsis';
+    sty.webkitBoxOrient = 'vertical';
+    sty.display = '-webkit-box';
+    sty.webkitLineClamp = clampValue as string;
+    if (isCSSValue) {
+      sty.height = options.clamp + 'px';
     }
-
-    // CONSTRUCTOR ________________________________________________________________
-    if (clampValue == 'auto') {
-        clampValue = getMaxLines(element);
+  } else {
+    const height = getMaxHeight(element, clampValue as number);
+    if (height <= element.clientHeight) {
+      clamped = truncate(
+        getLastChild(element, options),
+        element,
+        truncationHTMLContainer,
+        height,
+        options
+      );
     }
-    else if (isCSSValue) {
-        clampValue = getMaxLines(element, parseInt(clampValue as string));
-    }
-    let clamped: string;
-    if (supportsNativeClamp && options.useNativeClamp) {
-        sty.overflow = 'hidden';
-        sty.textOverflow = 'ellipsis';
-        sty.webkitBoxOrient = 'vertical';
-        sty.display = '-webkit-box';
-        sty.webkitLineClamp = clampValue as string;
-        if (isCSSValue) {
-            sty.height = options.clamp + 'px';
-        }
-    }
-    else {
-        const height = getMaxHeight(element, clampValue as number);
-        if (height <= element.clientHeight) {
-            clamped = truncate(getLastChild(element, options), element, truncationHTMLContainer, height, options);
-        }
-    }
-    return {
-        original,
-        clamped
-    };
+  }
+  return {
+    original,
+    clamped,
+  };
 }
